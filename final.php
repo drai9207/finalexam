f05032b22a00409ec0c5aaf65f1a4bb25433d7a0
drai9207 <115499226+drai9207@users.noreply.github.com>
2022-12-16 20:16:24 -0500

    Add files via upload

---
+++ final.php
@@ -0,0 +1,31 @@
+<?php
+
+class Final{
+        //properties
+        public $year;
+        public $GPA;
+        //predefined properties
+        public $totalAssignments = 4;
+        public $TotalStudyHours = 6;
+        public $TotalMaterials = 6;
+        //properties with no defined values
+        public $Professor;
+        public $Course;
+        public $Schedule;
+        
+        public $TotalMaterials = [
+            "Pencils" => 3,
+            "TextBooks" => 3,
+            "Course" => "WebProgramming"
+        ];
+        public $GPA = array(
+            "Above80" => True,
+            "Below80" => False,
+            "Above60" => False,
+            "Below60" => False,
+        );
+       
+      
+}
+
+?>
